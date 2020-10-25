b18eb34a6dca03b702b5caa77e35dc13bc60625e
EdwardDobson <47003895+EdwardDobson@users.noreply.github.com>
2020-10-25 13:04:04 +0000

    Update
    -Added wall jumping. Works with both normal sprites and tile maps with the layer mask called Wall.
    How it works
    -------------
    To jump left of a wall hold right to stick to the wall and hold left then press space.

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
@@ -14,42 +15,157 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField]
     float m_airSpeedMultiplier;
     GroundCheck m_groundCheck;
-
+    [SerializeField]
+    Vector3 m_jumpStretchSize;
+    float m_horizontal;
+    public Transform m_frontCheck;
+    bool m_grabbedWall;
+    bool m_wallSliding;
+    bool m_isJumping;
+    [SerializeField]
+    float m_wallSlidingSpeed;
+    LayerMask m_wallMask;
+    [SerializeField]
+    float m_checkRadius;
+    bool m_wallJumping;
+    [SerializeField]
+    float m_xWallForce;
+    [SerializeField]
+    float m_yWallForce;
+    [SerializeField]
+    float m_wallJumpTime;
+    [SerializeField]
+    float m_maxVelo;
     // Start is called before the first frame update
     void Start()
     {
         m_rb2d = GetComponent<Rigidbody2D>();
         m_groundCheck = GetComponent<GroundCheck>();
+        m_wallMask = LayerMask.GetMask("Wall");
     }
 
     // Update is called once per frame
     void Update()
     {
+        m_horizontal = Input.GetAxis("Horizontal");
+        if (Input.GetKeyDown(KeyCode.Space) && m_groundCheck.isGrounded() && !m_isJumping)
+        {
+            m_isJumping = true;
+        }
+ 
+        WallJumping();
+        SquishEffectJump();
+
+
+    }
+    private void FixedUpdate()
+    {
         if (m_groundCheck.isGrounded() == true)
         {
             MoveOnGround();
         }
-        else MoveInAir();
-
-        Jump();
+        else
+        {
+            MoveInAir();
+        }
+        if (m_isJumping)
+            Jump();
+        WallGrab();
     }
     void MoveOnGround()
     {
-        float H = Input.GetAxis("Horizontal");
-        m_rb2d.AddForce(new Vector2(H, 0) * m_speed * m_groundSpeedMultiplier);
+
+        m_rb2d.AddForce(new Vector2(m_horizontal, 0) * m_speed * m_groundSpeedMultiplier);
+        m_rb2d.velocity = Vector2.ClampMagnitude(m_rb2d.velocity, m_maxVelo);
+        if (m_horizontal < 0)
+        {
+            m_frontCheck.position = new Vector2(transform.position.x - 0.1f, transform.position.y);
+        }
+        else
+        {
+            m_frontCheck.position = new Vector2(transform.position.x + 0.1f, transform.position.y);
+        }
     }
     void MoveInAir()
     {
-        float H = Input.GetAxis("Horizontal");
-        m_rb2d.AddForce(new Vector2(H, 0) * m_speed * m_airSpeedMultiplier);
+
+        m_rb2d.AddForce(new Vector2(m_horizontal, 0) * m_speed * m_airSpeedMultiplier);
+ 
+        if (m_horizontal < 0)
+        {
+            m_frontCheck.position = new Vector2(transform.position.x - 0.1f, transform.position.y);
+
+        }
+        else
+        {
+            m_frontCheck.position = new Vector2(transform.position.x + 0.1f, transform.position.y);
+        }
     }
     void Jump()
     {
-        if(Input.GetKeyDown(KeyCode.Space) && m_groundCheck.isGrounded() == true)
+
+        Debug.Log("Jumping");
+        m_groundCheck.setIsGround(false);
+
+        m_rb2d.AddForce(new Vector2(0, m_jumpForce));
+        m_rb2d.velocity = Vector2.ClampMagnitude(m_rb2d.velocity, m_maxVelo);
+        SquishEffectJump();
+        m_isJumping = false;
+
+    }
+
+    void SquishEffectJump()
+    {
+        if (m_groundCheck.isGrounded())
         {
-            Debug.Log("Jumping");
-            m_groundCheck.setIsGround(false);
-            m_rb2d.AddForce(new Vector2(0, m_jumpForce));
+            transform.localScale = new Vector3(1, 1, 1);
         }
+        else
+        {
+            transform.localScale = new Vector3(transform.localScale.x, Mathf.Lerp(transform.localScale.y, m_jumpStretchSize.y, transform.localScale.y), transform.localScale.z);
+        }
+    }
+
+    void WallGrab()
+    {
+        m_grabbedWall = Physics2D.OverlapCircle(m_frontCheck.position, m_checkRadius, m_wallMask);
+
+        if (m_grabbedWall && !m_groundCheck.isGrounded() && m_horizontal != 0)
+        {
+            m_wallSliding = true;
+
+        }
+        else
+        {
+            m_wallSliding = false;
+        }
+        if (m_wallSliding)
+        {
+            m_rb2d.velocity = new Vector2(m_rb2d.velocity.x, Mathf.Clamp(m_rb2d.velocity.y, -m_wallSlidingSpeed, float.MaxValue));
+        }
+        if (m_wallJumping)
+        {
+            if(Input.GetKey(KeyCode.A))
+            {
+                m_rb2d.velocity = new Vector2(m_xWallForce * Vector2.left.x, m_yWallForce);
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                m_rb2d.velocity = new Vector2(m_xWallForce * Vector2.right.x, m_yWallForce);
+            }
+        }
+        Debug.Log("Wall Grabbing");
+    }
+    void WallJumping()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && m_wallSliding)
+        {
+            m_wallJumping = true;
+            Invoke("SetWallJumpingFalse", m_wallJumpTime);
+        }
+    }
+    void SetWallJumpingFalse()
+    {
+        m_wallJumping = false;
     }
 }
---
+++ Assets/WallDetection.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallDetection : MonoBehaviour
+{
+    public GameObject wall;
+    public Vector2 newPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag.Contains("Wall"))
+        {
+            wall = collision.gameObject;
+        }
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag.Contains("Wall"))
+        {
+            wall = collision.gameObject;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag.Contains("Wall"))
+        {
+            wall = null;
+        }
+    }
+}

7d07f99d4a2ee60b38908896fe42c418960b903a
EdwardDobson <47003895+EdwardDobson@users.noreply.github.com>
2020-11-20 14:39:35 +0000

    Update
    -Added sound + particle effects to the score

---
+++ Assets/Scripts/Player/AudioDetection.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioDetection : MonoBehaviour
+{
+    public AudioSource Source;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag.Contains("Score"))
+        {
+            Source.PlayOneShot(Source.clip);
+         GameObject newGameObject =  Instantiate(collision.transform.GetChild(0).gameObject, null);
+            newGameObject.transform.position = collision.transform.position;
+            newGameObject.GetComponent<ParticleSystem>().Play();
+        }
+    }
+}

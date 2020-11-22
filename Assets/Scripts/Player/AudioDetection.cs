f74e2f3f86ae3919279706e995d031e481000958
EdwardDobson <47003895+EdwardDobson@users.noreply.github.com>
2020-11-22 19:35:35 +0000

    Update
    -Added sound effects for health and shield pickups plus effects.
    -Added mini videos and descrptions on how to use some of the games controls

--- Assets/Scripts/Player/AudioDetection.cs
+++ Assets/Scripts/Player/AudioDetection.cs
@@ -5,25 +5,41 @@ using UnityEngine;
 public class AudioDetection : MonoBehaviour
 {
     public AudioSource Source;
-    // Start is called before the first frame update
-    void Start()
+    public AudioClip[] Clips;
+    PlayerStats m_playerStats;
+    private void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        m_playerStats = GetComponent<PlayerStats>();
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.tag.Contains("Score"))
         {
-            Source.PlayOneShot(Source.clip);
-         GameObject newGameObject =  Instantiate(collision.transform.GetChild(0).gameObject, null);
+            Source.PlayOneShot(Clips[0]);
+            GameObject newGameObject = Instantiate(collision.transform.GetChild(0).gameObject, null);
             newGameObject.transform.position = collision.transform.position;
             newGameObject.GetComponent<ParticleSystem>().Play();
+        }
+        if (collision.tag.Contains("Health"))
+        {
+            if (collision.transform.childCount > 0 && m_playerStats.GetCurrentHealth() < m_playerStats.GetMaxHealth())
+            {
+                Source.PlayOneShot(Clips[1]);
+                GameObject newGameObject = Instantiate(collision.transform.GetChild(0).gameObject, null);
+                newGameObject.transform.position = collision.transform.position;
+                newGameObject.GetComponent<ParticleSystem>().Play();
+            }
+        
+        }
+        if (collision.tag.Contains("Shield"))
+        {
+            if (collision.transform.childCount > 0 && m_playerStats.GetCurrentShield() < m_playerStats.GetMaxShield())
+            {
+                Source.PlayOneShot(Clips[2]);
+                GameObject newGameObject = Instantiate(collision.transform.GetChild(0).gameObject, null);
+                newGameObject.transform.position = collision.transform.position;
+                newGameObject.GetComponent<ParticleSystem>().Play();
+            }
         }
     }
 }
---
+++ Assets/Scripts/Utility/ControlSettings.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+public enum ControlsMenuDirection
+{
+    None,
+    Left,
+    Right
+}
+public class ControlSettings : MonoBehaviour
+{
+    public RawImage VideoImage;
+    public VideoPlayer VideoPlayer;
+    public VideoClip[] VideoClips;
+    public ControlsMenuDirection Direction;
+    public TextMeshProUGUI Title;
+    public TextMeshProUGUI Description;
+    string[] Descriptions = new string[] { "Press Space to jump.", "Press Q to grapple.", "Press A or D while on a wall." };
+    [SerializeField]
+    int m_index;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Direction = ControlsMenuDirection.None;
+        m_index = 0;
+        Title.text = VideoPlayer.clip.name;
+        Description.text = Descriptions[m_index];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void SwitchVideo()
+    {
+        switch(Direction)
+        {
+            case ControlsMenuDirection.Left:
+                m_index -= 1;
+                if (m_index < 0)
+                    m_index = VideoClips.Length-1;
+                VideoPlayer.clip = VideoClips[m_index];
+                Title.text = VideoPlayer.clip.name;
+                Description.text = Descriptions[m_index];
+                break;
+            case ControlsMenuDirection.Right:
+                m_index += 1;
+                if (m_index >= VideoClips.Length)
+                    m_index = 0;
+                VideoPlayer.clip = VideoClips[m_index];
+                Title.text = VideoPlayer.clip.name;
+                Description.text = Descriptions[m_index];
+                break;
+            default:
+                break;
+        }
+    }
+    public void SetDirection(string _direction)
+    {
+      if(_direction == "Left")
+        {
+            Direction = ControlsMenuDirection.Left;
+        }
+        if (_direction == "Right")
+        {
+            Direction = ControlsMenuDirection.Right;
+        }
+    }
+}
